58a952d2d4e2f12644355e696a9b1a1732097322
Gregory Z. Szucs <gregory@unipile.com>
2024-08-28 16:06:36 +0200

    Redraft InvalidResponseTypeError message, include first error in message, matching API behaviour

--- src/errors/request.error.ts
+++ src/errors/request.error.ts
@@ -9,9 +9,17 @@ export class UnsuccessfulRequestError extends UnipileError {
 
 export class InvalidResponseTypeError extends UnipileError {
   constructor(errorIterator: ValueErrorIterator) {
+    const body = Array.from(errorIterator);
     super({
-      message: 'The response type is unexpected. You can modify the validator or disable the validation to pass this issue.',
-      body: Array.from(errorIterator),
+      message: `Invalid response type : the response type didn't match the one expected by the SDK.
+
+Make sure the SDK is up to date. If the SDK is up to date and you still get the error, please contact our support.
+You may also disable the optional validation to ignore this issue and work with the current response.
+
+The full error list is available in the .body property of this error.
+Here is the first error :
+${JSON.stringify(body[0], null, 2)}`,
+      body,
     });
   }
 }
