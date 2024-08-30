e001501ef25fe7b250e59ac5274c70ba4eaa0a01
mlp-dw <mlp.dwwb@gmail.com>
2024-08-30 12:34:31 +0200

    adds email sub-methods in email.spec

--- src/tests-e2e/email.spec.ts
+++ src/tests-e2e/email.spec.ts
@@ -105,6 +105,313 @@ describe("EmailResource", () => {
         // }
       },
     );
+    it(
+      "should return a validated Email " + "on getOne.byId " + "when mail Id",
+      async () => {
+        // try {
+
+        const accounts = await client.account.getAll();
+        //   accounts.items = [accounts.items[0]];
+        //   console.log(
+        //     ...AccountListResponseValidator.Errors(accounts),
+        //     JSON.stringify(accounts.items[0], null, 2),
+        //   );
+        const account_id = accounts.items.filter(
+          (acc) =>
+            acc.type === "MAIL" ||
+            acc.type === "EXCHANGE" ||
+            acc.type === "GOOGLE_OAUTH" ||
+            acc.type === "OUTLOOK" ||
+            acc.type === "ICLOUD",
+        )[0].id;
+
+        const mail = await client.email.getAll({
+          account_id,
+          limit: 1,
+        });
+
+        //missing query options (e.g. unread)
+        const result = await client.email.getOne.byId({
+          email_id: mail.items[0].id,
+          account_id,
+        });
+        expect(result.object).toBe("Email");
+        // } catch (err) {
+        //   console.log(err);
+        //   throw err;
+        // }
+      },
+    );
+    it(
+      "should return a validated Email " +
+        "on getOne.byProviderId " +
+        "when provider_id",
+      async () => {
+        // try {
+
+        const accounts = await client.account.getAll();
+        //   accounts.items = [accounts.items[0]];
+        //   console.log(
+        //     ...AccountListResponseValidator.Errors(accounts),
+        //     JSON.stringify(accounts.items[0], null, 2),
+        //   );
+        const account_id = accounts.items.filter(
+          (acc) =>
+            acc.type === "MAIL" ||
+            acc.type === "EXCHANGE" ||
+            acc.type === "GOOGLE_OAUTH" ||
+            acc.type === "OUTLOOK" ||
+            acc.type === "ICLOUD",
+        )[0].id;
+
+        const mail = await client.email.getAll({
+          account_id,
+          limit: 1,
+        });
+
+        //missing query options (e.g. unread)
+        const result = await client.email.getOne.byProviderId({
+          email_provider_id: mail.items[0].id,
+          account_id,
+        });
+        expect(result.object).toBe("Email");
+        // } catch (err) {
+        //   console.log(err);
+        //   throw err;
+        // }
+      },
+    );
+  });
+  //----------------------------------------------------------------------------
+  describe("delete", () => {
+    //--------------------------------------------------------------------------
+    it(
+      "should return a validated EmailDeleted " + "on delete " + "when mail Id",
+      async () => {
+        // try {
+
+        const accounts = await client.account.getAll();
+        //   accounts.items = [accounts.items[0]];
+        //   console.log(
+        //     ...AccountListResponseValidator.Errors(accounts),
+        //     JSON.stringify(accounts.items[0], null, 2),
+        //   );
+        const account_id = accounts.items.filter(
+          (acc) =>
+            acc.type === "MAIL" ||
+            acc.type === "EXCHANGE" ||
+            acc.type === "GOOGLE_OAUTH" ||
+            acc.type === "OUTLOOK" ||
+            acc.type === "ICLOUD",
+        )[0].id;
+
+        const mail = await client.email.getAll({
+          account_id,
+          limit: 1,
+        });
+
+        const result = await client.email.delete(mail.items[0].id);
+        expect(result.object).toBe("EmailDeleted");
+        // } catch (err) {
+        //   console.log(err);
+        //   throw err;
+        // }
+      },
+    );
+    //--------------------------------------------------------------------------
+    it(
+      "should return a validated EmailDeleted " +
+        "on delete.byId " +
+        "when mail Id",
+      async () => {
+        // try {
+
+        const accounts = await client.account.getAll();
+        //   accounts.items = [accounts.items[0]];
+        //   console.log(
+        //     ...AccountListResponseValidator.Errors(accounts),
+        //     JSON.stringify(accounts.items[0], null, 2),
+        //   );
+        const account_id = accounts.items.filter(
+          (acc) =>
+            acc.type === "MAIL" ||
+            acc.type === "EXCHANGE" ||
+            acc.type === "GOOGLE_OAUTH" ||
+            acc.type === "OUTLOOK" ||
+            acc.type === "ICLOUD",
+        )[0].id;
+
+        const mail = await client.email.getAll({
+          account_id,
+          limit: 1,
+        });
+
+        const result = await client.email.delete.byId({
+          email_id: mail.items[0].id,
+          account_id,
+        });
+        expect(result.object).toBe("EmailDeleted");
+        // } catch (err) {
+        //   console.log(err);
+        //   throw err;
+        // }
+      },
+    );
+    //--------------------------------------------------------------------------
+    it(
+      "should return a validated EmailDeleted " +
+        "on delete.byProviderId " +
+        "when provider_id",
+      async () => {
+        // try {
+
+        const accounts = await client.account.getAll();
+        //   accounts.items = [accounts.items[0]];
+        //   console.log(
+        //     ...AccountListResponseValidator.Errors(accounts),
+        //     JSON.stringify(accounts.items[0], null, 2),
+        //   );
+        const account_id = accounts.items.filter(
+          (acc) =>
+            acc.type === "MAIL" ||
+            acc.type === "EXCHANGE" ||
+            acc.type === "GOOGLE_OAUTH" ||
+            acc.type === "OUTLOOK" ||
+            acc.type === "ICLOUD",
+        )[0].id;
+
+        const mail = await client.email.getAll({
+          account_id,
+          limit: 1,
+        });
+
+        const result = await client.email.delete.byProviderId({
+          email_provider_id: mail.items[0].id,
+          account_id,
+        });
+        expect(result.object).toBe("EmailDeleted");
+        // } catch (err) {
+        //   console.log(err);
+        //   throw err;
+        // }
+      },
+    );
+  });
+  //----------------------------------------------------------------------------
+  describe("update", () => {
+    //--------------------------------------------------------------------------
+    it(
+      "should return a validated EmailUpdated " + "on update " + "when mail Id",
+      async () => {
+        // try {
+
+        const accounts = await client.account.getAll();
+        //   accounts.items = [accounts.items[0]];
+        //   console.log(
+        //     ...AccountListResponseValidator.Errors(accounts),
+        //     JSON.stringify(accounts.items[0], null, 2),
+        //   );
+        const account_id = accounts.items.filter(
+          (acc) =>
+            acc.type === "MAIL" ||
+            acc.type === "EXCHANGE" ||
+            acc.type === "GOOGLE_OAUTH" ||
+            acc.type === "OUTLOOK" ||
+            acc.type === "ICLOUD",
+        )[0].id;
+
+        const mail = await client.email.getAll({
+          account_id,
+          limit: 1,
+        });
+
+        //missing query options (e.g. unread)
+        const result = await client.email.update(mail.items[0].id);
+        expect(result.object).toBe("EmailUpdated");
+        // } catch (err) {
+        //   console.log(err);
+        //   throw err;
+        // }
+      },
+    );
+    //--------------------------------------------------------------------------
+    it(
+      "should return a validated EmailUpdated " +
+        "on update.byId " +
+        "when mail Id",
+      async () => {
+        // try {
+
+        const accounts = await client.account.getAll();
+        //   accounts.items = [accounts.items[0]];
+        //   console.log(
+        //     ...AccountListResponseValidator.Errors(accounts),
+        //     JSON.stringify(accounts.items[0], null, 2),
+        //   );
+        const account_id = accounts.items.filter(
+          (acc) =>
+            acc.type === "MAIL" ||
+            acc.type === "EXCHANGE" ||
+            acc.type === "GOOGLE_OAUTH" ||
+            acc.type === "OUTLOOK" ||
+            acc.type === "ICLOUD",
+        )[0].id;
+
+        const mail = await client.email.getAll({
+          account_id,
+          limit: 1,
+        });
+
+        const result = await client.email.update.byId({
+          email_id: mail.items[0].id,
+          account_id,
+        });
+        expect(result.object).toBe("EmailUpdated");
+        // } catch (err) {
+        //   console.log(err);
+        //   throw err;
+        // }
+      },
+    );
+    //--------------------------------------------------------------------------
+    it(
+      "should return a validated EmailUpdated " +
+        "on update.byProviderId " +
+        "when provider_id ",
+      async () => {
+        // try {
+
+        const accounts = await client.account.getAll();
+        //   accounts.items = [accounts.items[0]];
+        //   console.log(
+        //     ...AccountListResponseValidator.Errors(accounts),
+        //     JSON.stringify(accounts.items[0], null, 2),
+        //   );
+        const account_id = accounts.items.filter(
+          (acc) =>
+            acc.type === "MAIL" ||
+            acc.type === "EXCHANGE" ||
+            acc.type === "GOOGLE_OAUTH" ||
+            acc.type === "OUTLOOK" ||
+            acc.type === "ICLOUD",
+        )[0].id;
+
+        const mail = await client.email.getAll({
+          account_id,
+          limit: 1,
+        });
+
+        const result = await client.email.update.byProviderId({
+          email_provider_id: mail.items[0].id,
+          account_id,
+        });
+        expect(result.object).toBe("EmailUpdated");
+        // } catch (err) {
+        //   console.log(err);
+        //   throw err;
+        // }
+      },
+    );
   });
   //----------------------------------------------------------------------------
   describe("getOneFolder", () => {
@@ -136,6 +443,82 @@ describe("EmailResource", () => {
         });
 
         const result = await client.email.getOneFolder(folders.items[0].id);
+        expect(result.object).toBe("Folder");
+        // } catch (err) {
+        //   console.log(err);
+        //   throw err;
+        // }
+      },
+    );
+    //--------------------------------------------------------------------------
+    it(
+      "should return a validated Folder " +
+        "on getOneFolder.byId " +
+        "when folder Id",
+      async () => {
+        // try {
+
+        const accounts = await client.account.getAll();
+        //   accounts.items = [accounts.items[0]];
+        //   console.log(
+        //     ...AccountListResponseValidator.Errors(accounts),
+        //     JSON.stringify(accounts.items[0], null, 2),
+        //   );
+        const account_id = accounts.items.filter(
+          (acc) =>
+            acc.type === "MAIL" ||
+            acc.type === "EXCHANGE" ||
+            acc.type === "GOOGLE_OAUTH" ||
+            acc.type === "OUTLOOK" ||
+            acc.type === "ICLOUD",
+        )[0].id;
+
+        const folders = await client.email.getAllFolders({
+          account_id,
+        });
+
+        const result = await client.email.getOneFolder.byId({
+          folder_id: folders.items[0].id,
+          account_id,
+        });
+        expect(result.object).toBe("Folder");
+        // } catch (err) {
+        //   console.log(err);
+        //   throw err;
+        // }
+      },
+    );
+    //--------------------------------------------------------------------------
+    it(
+      "should return a validated Folder " +
+        "on getOneFolder.byProviderId " +
+        "when folder_provider_id",
+      async () => {
+        // try {
+
+        const accounts = await client.account.getAll();
+        //   accounts.items = [accounts.items[0]];
+        //   console.log(
+        //     ...AccountListResponseValidator.Errors(accounts),
+        //     JSON.stringify(accounts.items[0], null, 2),
+        //   );
+        const account_id = accounts.items.filter(
+          (acc) =>
+            acc.type === "MAIL" ||
+            acc.type === "EXCHANGE" ||
+            acc.type === "GOOGLE_OAUTH" ||
+            acc.type === "OUTLOOK" ||
+            acc.type === "ICLOUD",
+        )[0].id;
+
+        const folders = await client.email.getAllFolders({
+          account_id,
+        });
+
+        const result = await client.email.getOneFolder.byProviderId({
+          folder_provider_id: folders.items[0].id,
+          account_id,
+        });
         expect(result.object).toBe("Folder");
         // } catch (err) {
         //   console.log(err);
@@ -188,6 +571,91 @@ describe("EmailResource", () => {
         // }
       },
     );
+    //--------------------------------------------------------------------------
+    it.skip(
+      "should return a validated Email Attachment " +
+        "on getEmailAttachment.byId " +
+        "when mail Id " +
+        "and attachment Id",
+      async () => {
+        // try {
+
+        const accounts = await client.account.getAll();
+        //   accounts.items = [accounts.items[0]];
+        //   console.log(
+        //     ...AccountListResponseValidator.Errors(accounts),
+        //     JSON.stringify(accounts.items[0], null, 2),
+        //   );
+        const account_id = accounts.items.filter(
+          (acc) =>
+            acc.type === "MAIL" ||
+            acc.type === "EXCHANGE" ||
+            acc.type === "GOOGLE_OAUTH" ||
+            acc.type === "OUTLOOK" ||
+            acc.type === "ICLOUD",
+        )[0].id;
+
+        const mail = await client.email.getAll({
+          account_id,
+          limit: 1,
+        });
+
+        const email_id = mail.items[0].id;
+        const attachment_id = ""; // don't know how to retrieve this
+        const result = await client.email.getEmailAttachment.byId({
+          email_id,
+          attachment_id,
+        });
+        expect(result.object).toBe("Email");
+        // } catch (err) {
+        //   console.log(err);
+        //   throw err;
+        // }
+      },
+    );
+    //--------------------------------------------------------------------------
+    it.skip(
+      "should return a validated Email Attachment " +
+        "on getEmailAttachment.byProviderId " +
+        "when provider_id " +
+        "and attachment Id",
+      async () => {
+        // try {
+
+        const accounts = await client.account.getAll();
+        //   accounts.items = [accounts.items[0]];
+        //   console.log(
+        //     ...AccountListResponseValidator.Errors(accounts),
+        //     JSON.stringify(accounts.items[0], null, 2),
+        //   );
+        const account_id = accounts.items.filter(
+          (acc) =>
+            acc.type === "MAIL" ||
+            acc.type === "EXCHANGE" ||
+            acc.type === "GOOGLE_OAUTH" ||
+            acc.type === "OUTLOOK" ||
+            acc.type === "ICLOUD",
+        )[0].id;
+
+        const mail = await client.email.getAll({
+          account_id,
+          limit: 1,
+        });
+
+        const email_provider_id = mail.items[0].provider_id;
+        const attachment_id = ""; // don't know how to retrieve this
+        const result = await client.email.getEmailAttachment.byProviderId({
+          email_provider_id,
+          attachment_id,
+          account_id,
+        });
+        expect(result.object).toBe("Email");
+        // } catch (err) {
+        //   console.log(err);
+        //   throw err;
+        // }
+      },
+    );
   });
   //----------------------------------------------------------------------------
   describe("send", () => {
@@ -229,79 +697,6 @@ describe("EmailResource", () => {
       },
     );
   });
+
   //----------------------------------------------------------------------------
-  describe("delete", () => {
-    //--------------------------------------------------------------------------
-    it.skip(
-      "should return a validated EmailDeleted " + "on delete " + "when mail Id",
-      async () => {
-        // try {
-
-        const accounts = await client.account.getAll();
-        //   accounts.items = [accounts.items[0]];
-        //   console.log(
-        //     ...AccountListResponseValidator.Errors(accounts),
-        //     JSON.stringify(accounts.items[0], null, 2),
-        //   );
-        const account_id = accounts.items.filter(
-          (acc) =>
-            acc.type === "MAIL" ||
-            acc.type === "EXCHANGE" ||
-            acc.type === "GOOGLE_OAUTH" ||
-            acc.type === "OUTLOOK" ||
-            acc.type === "ICLOUD",
-        )[0].id;
-
-        const mail = await client.email.getAll({
-          account_id,
-          limit: 1,
-        });
-
-        const result = await client.email.delete(mail.items[0].id);
-        expect(result.object).toBe("EmailDeleted");
-        // } catch (err) {
-        //   console.log(err);
-        //   throw err;
-        // }
-      },
-    );
-  });
-  //----------------------------------------------------------------------------
-  describe("update", () => {
-    //--------------------------------------------------------------------------
-    it.skip(
-      "should return a validated EmailUpdated " + "on update " + "when mail Id",
-      async () => {
-        // try {
-
-        const accounts = await client.account.getAll();
-        //   accounts.items = [accounts.items[0]];
-        //   console.log(
-        //     ...AccountListResponseValidator.Errors(accounts),
-        //     JSON.stringify(accounts.items[0], null, 2),
-        //   );
-        const account_id = accounts.items.filter(
-          (acc) =>
-            acc.type === "MAIL" ||
-            acc.type === "EXCHANGE" ||
-            acc.type === "GOOGLE_OAUTH" ||
-            acc.type === "OUTLOOK" ||
-            acc.type === "ICLOUD",
-        )[0].id;
-
-        const mail = await client.email.getAll({
-          account_id,
-          limit: 1,
-        });
-
-        //missing query options (e.g. unread)
-        const result = await client.email.update(mail.items[0].id);
-        expect(result.object).toBe("EmailUpdated");
-        // } catch (err) {
-        //   console.log(err);
-        //   throw err;
-        // }
-      },
-    );
-  });
 });
